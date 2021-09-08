aab4e1cf7573e3e44df42173d3d75d5ab392878c
eliran <manzeli@me.com>
2021-09-08 23:25:42 +0300

    category schema

---
+++ models/Category.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const categorySchema = mongoose.Schema({
+    categoryName: {
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    image:{
+        type: String,
+        
+    },
+    categoryId:{
+        type:String,
+    }
+
+})
+module.exports=mongoose.model('category', categorySchema)
